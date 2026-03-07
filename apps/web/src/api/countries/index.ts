67fe591eaa1d0b90f0b32b03bf2b46d837f54d78
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2026-03-07 17:56:12 +0300

    feat(web): add CRUD operations for countries

--- apps/web/src/api/countries/index.ts
+++ apps/web/src/api/countries/index.ts
@@ -2,5 +2,13 @@ export {
 	type CreateCountryInput,
 	createCountry,
 	createCountryOptions,
+	deleteCountry,
+	deleteCountryOptions,
+	type UpdateCountryInput,
+	updateCountry,
+	updateCountryOptions,
+	useCreateCountry,
+	useDeleteCountry,
+	useUpdateCountry,
 } from "./mutations";
 export { countriesOptions, getCountries, useCountries } from "./queries";
--- apps/web/src/api/countries/mutations.ts
+++ apps/web/src/api/countries/mutations.ts
@@ -1,11 +1,15 @@
 import type { Treaty } from "@elysiajs/eden";
-import { mutationOptions } from "@tanstack/react-query";
+import { mutationOptions, useMutation } from "@tanstack/react-query";
 import { client } from "@web/lib/api-client";
 
 export type CreateCountryInput = Pick<
 	Treaty.Data<typeof client.api.countries.post>,
 	"name" | "code" | "currency"
 >;
+
+export type UpdateCountryInput = CreateCountryInput & {
+	countryId: string;
+};
 
 export async function createCountry({
 	name,
@@ -27,9 +31,72 @@ export async function createCountry({
 	return response.data;
 }
 
+export async function updateCountry({
+	countryId,
+	name,
+	code,
+	currency,
+}: UpdateCountryInput) {
+	const response = await client.api.countries({ id: countryId }).put({
+		name,
+		code,
+		currency,
+	});
+
+	if (response.error) {
+		throw new Error(
+			response.error.status === 422 ? "Validation error" : response.error.value,
+		);
+	}
+
+	return response.data;
+}
+
+export async function deleteCountry({ countryId }: { countryId: string }) {
+	const response = await client.api.countries({ id: countryId }).delete();
+
+	if (response.error) {
+		throw new Error(
+			response.error.status === 422 ? "Validation error" : response.error.value,
+		);
+	}
+
+	return response.data;
+}
+
+// ============ OPTIONS =============
+
 export function createCountryOptions() {
 	return mutationOptions({
 		mutationKey: ["createCountry"],
 		mutationFn: createCountry,
 	});
+}
+
+export function updateCountryOptions() {
+	return mutationOptions({
+		mutationKey: ["updateCountry"],
+		mutationFn: updateCountry,
+	});
+}
+
+export function deleteCountryOptions() {
+	return mutationOptions({
+		mutationKey: ["deleteCountry"],
+		mutationFn: deleteCountry,
+	});
+}
+
+// =========== HOOKS =============
+
+export function useCreateCountry() {
+	return useMutation(createCountryOptions());
+}
+
+export function useUpdateCountry() {
+	return useMutation(updateCountryOptions());
+}
+
+export function useDeleteCountry() {
+	return useMutation(deleteCountryOptions());
 }
